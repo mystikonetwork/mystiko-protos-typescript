7b6890a1365a6ca9fa80a1ec87fb1b5ef1b65a51
Fitch Li <fitch@mystiko.network>
2024-04-18 12:30:15 +0800

    fix missing MerkleTree pb file (#20)

--- src/data/v1/index.ts
+++ src/data/v1/index.ts
@@ -1,4 +1,5 @@
 export * from '../../gen/mystiko/data/v1/chain_pb';
 export * from '../../gen/mystiko/data/v1/contract_pb';
 export * from '../../gen/mystiko/data/v1/commitment_pb';
+export * from '../../gen/mystiko/data/v1/merkle_pb';
 export * from '../../gen/mystiko/data/v1/nullifier_pb';
