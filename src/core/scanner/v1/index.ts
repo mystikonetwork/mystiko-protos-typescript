94e375e0e491fde17df7b77bab96db0801cc4c2e
ericlurker <96100006+ericlurker@users.noreply.github.com>
2024-12-05 15:06:34 +0800

    update, package version to 0.5.11 (#24)

--- src/core/scanner/v1/index.ts
+++ src/core/scanner/v1/index.ts
@@ -2,3 +2,4 @@ export * from '../../../gen/mystiko/core/scanner/v1/asset_pb';
 export * from '../../../gen/mystiko/core/scanner/v1/balance_pb';
 export * from '../../../gen/mystiko/core/scanner/v1/reset_pb';
 export * from '../../../gen/mystiko/core/scanner/v1/scan_pb';
+export * from '../../../gen/mystiko/core/scanner/v1/import_pb';
